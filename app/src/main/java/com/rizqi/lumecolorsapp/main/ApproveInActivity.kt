6fc3f3a4114efa02f27f22de18d074507a988f4a
RizqiSatriaW <rizqi.satria@majoo.id>
2021-11-13 14:30:25 +0700

    Approve In

--- app/src/main/java/com/rizqi/lumecolorsapp/main/ApproveInActivity.kt
+++ app/src/main/java/com/rizqi/lumecolorsapp/main/ApproveInActivity.kt
@@ -8,5 +8,13 @@ class ApproveInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_approve_in)
+
+// if status Pending (ketika baru tampil dari loading IN)
+// if status Approve (ketika sudah input edit text qty_pass dan kondisi == qty_input || qty_pass != 0 dan visible approve view )
+// if status Reject (ketika sudah input edit text qty_pass == 0 dan visible reject_view, visible button_show_detail )
+// kondisi awal pending, jika ganti kondisi maka pending_view di gone dan visible view reject / approve
+
+// Qty Reject di dapat dengan perhitungan qty_input loadingIn - qty_approve
+
     }
 }
--- app/src/main/java/com/rizqi/lumecolorsapp/main/MenuActivity.kt
+++ app/src/main/java/com/rizqi/lumecolorsapp/main/MenuActivity.kt
@@ -2,10 +2,12 @@ package com.rizqi.lumecolorsapp.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.rizqi.lumecolorsapp.R
+import com.rizqi.lumecolorsapp.utils.Constants
 import com.rizqi.lumecolorsapp.utils.Constants.CHECKER
 import com.rizqi.lumecolorsapp.utils.Constants.SP_LEVEL
 import com.rizqi.lumecolorsapp.utils.SharedPreferencesUtils
@@ -14,12 +16,18 @@ class MenuActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferencesUtils
 
     private lateinit var mLevel: TextView
-    private lateinit var loadingIn: RelativeLayout
+    private lateinit var history: RelativeLayout
     private lateinit var approveIn: RelativeLayout
     private lateinit var approveOut: RelativeLayout
     private lateinit var stockIn: RelativeLayout
     private lateinit var stockOut: RelativeLayout
     private lateinit var stockOpname: RelativeLayout
+    private lateinit var hiddenStockOpname: View
+    private lateinit var hiddenStockOut: View
+    private lateinit var hiddenStockIn: View
+    private lateinit var hiddenApproveIn: View
+    private lateinit var hiddenApproveOut: View
+    private lateinit var hiddenHistory: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,22 +38,39 @@ class MenuActivity : AppCompatActivity() {
 
 //        Define ID
         mLevel = findViewById(R.id.level)
-        loadingIn = findViewById(R.id.card_loading_in)
+        history = findViewById(R.id.card_history)
         approveIn = findViewById(R.id.card_approve_in)
         approveOut = findViewById(R.id.card_approve_out)
         stockIn = findViewById(R.id.card_stock_in)
         stockOut = findViewById(R.id.card_stock_out)
         stockOpname = findViewById(R.id.card_stock_opname)
+        hiddenStockOpname = findViewById(R.id.hidden_stock_opname)
+        hiddenStockIn = findViewById(R.id.hidden_stock_in)
+        hiddenStockOut = findViewById(R.id.hidden_stock_out)
+        hiddenApproveIn = findViewById(R.id.hidden_approve_in)
+        hiddenApproveOut = findViewById(R.id.hidden_approve_out)
 
 
 //        Set Variable SharedPreferences
         mLevel.text = sharedPreferences.getStringSharedPreferences(SP_LEVEL)
 
+//        Set LEVEL Access
+        if (SP_LEVEL == "CHECKER"){
+            hiddenApproveIn.visibility
+        }
+        if (SP_LEVEL == "PACKING" || SP_LEVEL == "SENDER"){
+            hiddenHistory.visibility
+            hiddenApproveIn.visibility
+            hiddenStockIn.visibility
+            hiddenStockOut.visibility
+            hiddenStockOpname.visibility
+        }
 
 //        Action
-        loadingIn.setOnClickListener {
+        history.setOnClickListener {
             val intent = Intent(this@MenuActivity, LoadingInActivity::class.java)
             this@MenuActivity.startActivity(intent)
         }
+
     }
 }
