da4df709eeec2652681976b0144f4fbea8db8ceb
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2021-11-15 01:22:32 +0700

    (+) Show Dialog Logout (+) Logout Handler

--- app/src/main/java/com/rizqi/lumecolorsapp/main/MenuActivity.kt
+++ app/src/main/java/com/rizqi/lumecolorsapp/main/MenuActivity.kt
@@ -1,12 +1,15 @@
 package com.rizqi.lumecolorsapp.main
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.view.Window
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.rizqi.lumecolorsapp.R
+import com.rizqi.lumecolorsapp.auth.LoginActivity
 import com.rizqi.lumecolorsapp.utils.Constants
 import com.rizqi.lumecolorsapp.utils.Constants.CHECKER
 import com.rizqi.lumecolorsapp.utils.Constants.SP_LEVEL
@@ -16,6 +19,7 @@ class MenuActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferencesUtils
 
     private lateinit var mLevel: TextView
+    private lateinit var btnLogout: ImageView
     private lateinit var history: RelativeLayout
     private lateinit var approveIn: RelativeLayout
     private lateinit var approveOut: RelativeLayout
@@ -38,6 +42,7 @@ class MenuActivity : AppCompatActivity() {
 
 //        Define ID
         mLevel = findViewById(R.id.level)
+        btnLogout = findViewById(R.id.button_logout)
         history = findViewById(R.id.card_history)
 //        approveIn = findViewById(R.id.card_approve_in)
         approveOut = findViewById(R.id.card_approve_out)
@@ -92,5 +97,46 @@ class MenuActivity : AppCompatActivity() {
             this@MenuActivity.startActivity(intent)
         }
 
+        btnLogout.setOnClickListener {
+            showDialogLogout()
+        }
+
+    }
+
+    private fun showDialogLogout() {
+        val dialog = Dialog(this@MenuActivity)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.dialog_logout)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+
+        val btnNo = dialog.findViewById<Button>(R.id.button_no)
+        val btnYes = dialog.findViewById<Button>(R.id.button_yes)
+
+        btnNo.setOnClickListener { dialog.dismiss() }
+        btnYes.setOnClickListener { processLogout(dialog) }
+
+        dialog.show()
+    }
+
+    private fun processLogout(dialog: Dialog) {
+        setDataUser(SP_LEVEL, 0, "")
+        setDataUser(Constants.PERIODE, 0, "")
+        setDataUser(Constants.LOGGED_STATE, 0, Constants.LOGGED_OUT)
+
+        dialog.dismiss()
+
+        val intent = Intent(this@MenuActivity, LoginActivity::class.java)
+        this@MenuActivity.startActivity(intent)
+        finish()
+        
+    }
+
+    fun setDataUser(key: String, int: Int, string: String){
+        if (string != ""){
+            sharedPreferences.setSharedPreferences(key, string)
+        } else {
+            sharedPreferences.setSharedPreferences(key, int)
+        }
     }
 }
