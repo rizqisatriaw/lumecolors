7e6caa7b143d7ecdc266d10581dbf6bcc1f33b12
rizqisatriaw <rizqi.satria@majoo.id>
2022-06-14 23:51:15 +0700

    full

---
+++ app/src/main/java/com/rizqi/lumecolorsapp/helper/PrinterHelper.java
@@ -0,0 +1,30 @@
+//package com.rizqi.lumecolorsapp.helper;
+//
+//import android.util.Log;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class PrinterHelper {
+//
+//    private List<ArrayList<String>> dataToPrintList = new ArrayList<>();
+//    public PrinterHelper testPrint() {
+//
+//        processIndex = 0;
+//        Long idPrinter = 0L;
+//
+//
+//
+//        return this;
+//    }
+//
+//
+//    private PrinterHelper addLine(String line) {
+//        if (!line.isEmpty()) {
+//            dataToPrintList.get(processIndex).add(line);
+//        } else {
+//            LF();
+//        }
+//        return this;
+//    }
+//}
